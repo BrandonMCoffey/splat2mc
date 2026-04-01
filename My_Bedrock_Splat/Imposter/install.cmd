dfdc2f293665eec9628a46f3ef879e28810c1cf6
BrandonMCoffey <bmc939@gmail.com>
2026-03-31 22:16:53 -0500

    Imposter Test 2

---
+++ My_Bedrock_Splat/Imposter/install.cmd
@@ -0,0 +1,2 @@
+xcopy /d /y /e Imposter_BP "%appdata%\Minecraft Bedrock\Users\Shared\games\com.mojang\development_behavior_packs\Imposter_BP\"
+xcopy /d /y /e Imposter_RP "%appdata%\Minecraft Bedrock\Users\Shared\games\com.mojang\development_resource_packs\Imposter_RP\"
