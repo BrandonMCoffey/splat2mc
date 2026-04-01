216abf8e89ecf5eb43a3e7e13fffb0ef6aeebd04
BrandonMCoffey <bmc939@gmail.com>
2026-03-31 21:46:01 -0500

    Cleanup file organization

---
+++ My_Bedrock_Splat/SplatImposter/install.cmd
@@ -0,0 +1,2 @@
+xcopy /d /y /e SplatImposter_BP "%appdata%\Minecraft Bedrock\Users\Shared\games\com.mojang\development_behavior_packs\SplatImposter_BP\"
+xcopy /d /y /e SplatImposter_RP "%appdata%\Minecraft Bedrock\Users\Shared\games\com.mojang\development_resource_packs\SplatImposter_RP\"
---
+++ My_Bedrock_Splat/SplatRenderer/install.cmd
@@ -0,0 +1,2 @@
+xcopy /d /y /e SplatRenderer_BP "%appdata%\Minecraft Bedrock\Users\Shared\games\com.mojang\development_behavior_packs\SplatRenderer_BP\"
+xcopy /d /y /e SplatRenderer_RP "%appdata%\Minecraft Bedrock\Users\Shared\games\com.mojang\development_resource_packs\SplatRenderer_RP\"
